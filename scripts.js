9651f9fd105f930ecfee04ddd46fbf995d1b8c77
Santino Casado <santinocasado05@gmail.com>
2025-07-18 15:22:38 -0600

    Agrego nuevas etiquetas y enlaces con js y css

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+/*Script que utiliza la funcion show alert, 
+pasandole el parametro del nombre de la plataforma */
+
+function showAlert(platform) {
+  alert("Opening your " + platform + " profile...");
+}
