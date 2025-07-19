f8801ffc1c56b42ba4cc0ef098a349a602c15e46
Santino Casado <santinocasado05@gmail.com>
2025-07-19 13:38:38 -0600

    edits

--- scripts.js
+++ scripts.js
@@ -2,6 +2,24 @@
 /*Script que utiliza la funcion show alert, 
 pasandole el parametro del nombre de la plataforma */
 
-function showAlert(platform) {
-  alert("Opening your " + platform + " profile...");
+function showAlertLinks(platform) {
+  alert("Opening the " + platform + " profile...");
+}
+
+function showAlertSkills(platform) {
+  alert("Opening the " + platform + " original page");
+}
+
+
+function showAlertDownload(archive, event) {
+  event.preventDefault(); // evita que el link se abra antes
+
+  const response = confirm("Do you want to download the " + archive + "?");
+
+  if (response) {
+    // solo si acepta, se inicia la descarga
+    window.location.href = "pdf/TUP_Plan-de-Estudio-2024.pdf";
+  } else {
+    alert("Download cancelled.");
+  }
 }
